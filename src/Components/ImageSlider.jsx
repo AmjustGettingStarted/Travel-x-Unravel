00761a664314f27fa0f71cae9a2dc3fb907c1ead
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-04-04 12:06:19 +0530

    ImageSlider height 780 to 580 reduced

--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -48,7 +48,7 @@ const ImageSlider = () => {
     setCurrentIndex(slideIndex);
   };
   return (
-    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16  px-4 relative group">
+    <div className="max-w-[1400px] h-[580px] w-full m-auto py-16  px-4 relative group">
       <div
         title={slides[currentIndex].title}
         className=" w-full h-full  rounded-2xl bg-center bg-cover duration-500 uppercase"
